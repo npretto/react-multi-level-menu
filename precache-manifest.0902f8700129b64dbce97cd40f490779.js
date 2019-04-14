07379529651d3079177fef894a7944dbc716d9a4
npretto <info@npretto.com>
2019-04-14 14:04:40 +0200

    Updates

---
+++ precache-manifest.0902f8700129b64dbce97cd40f490779.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b67fd8af8edc99ba1e7",
+    "url": "/react-multi-level-menu/static/css/main.10c508d1.chunk.css"
+  },
+  {
+    "revision": "8b67fd8af8edc99ba1e7",
+    "url": "/react-multi-level-menu/static/js/main.13bf52ac.chunk.js"
+  },
+  {
+    "revision": "965d28839280c5ee5146",
+    "url": "/react-multi-level-menu/static/js/runtime~main.b607ec9d.js"
+  },
+  {
+    "revision": "3abbfc053e7bf7f41d37",
+    "url": "/react-multi-level-menu/static/css/2.bfaca08c.chunk.css"
+  },
+  {
+    "revision": "3abbfc053e7bf7f41d37",
+    "url": "/react-multi-level-menu/static/js/2.f191e027.chunk.js"
+  },
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "/react-multi-level-menu/static/media/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "/react-multi-level-menu/static/media/open-iconic.527eff64.otf"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "/react-multi-level-menu/static/media/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "/react-multi-level-menu/static/media/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "eca2c26759a009a4a9361151efe99e74",
+    "url": "/react-multi-level-menu/static/media/open-iconic.eca2c267.svg"
+  },
+  {
+    "revision": "e37fc95197422b43f5ba1caa1c53a09e",
+    "url": "/react-multi-level-menu/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-multi-level-menu/precache-manifest.0902f8700129b64dbce97cd40f490779.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-multi-level-menu/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
